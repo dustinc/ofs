95bd4d1a40b21d523bdf6ccaf418f769aaba1f2e
Dustin <coffey.dustin@gmail.com>
2012-08-06 17:09:50 -0400

    Add basic user routes.

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,18 @@
 module.exports = function(app) {
   
   var home = require('../app/controllers/home_controller')(app);
+  var user = require('../app/controllers/user')(app);
   
   var db = app.set('db');
   
   app.get('/', home.index);
 
   app.get('/about', home.load.bind(null, 'about'));
+
+  app.get('/user', user.index);
+  app.get('/user/create', user.create);
+  app.get('/user/:id', user.index);
+  app.get('/user/:id/edit', user.edit);
+  app.get('/user/:id/delete', user.delete);
 };
 
