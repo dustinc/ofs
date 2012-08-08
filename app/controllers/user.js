ddbcecb871ead6ad889e35380d1d561ee9c3796e
Dustin <coffey.dustin@gmail.com>
2012-08-08 17:48:47 -0400

    Update user routes, controller, and views.
    Add basic create/update/remove functionality, simple comments, and 'proper' returns.

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,8 +1,11 @@
 
+// globals
+
 var controller = {},
     app,
     db;
 
+// constructor
 
 module.exports = function(_app) {
   app = _app;
@@ -10,20 +13,71 @@ module.exports = function(_app) {
   return controller;
 };
 
+// index
+
 controller.index = function(req, res, next) {
-  res.render('user/index',
+  return res.render('user/index',
     {users: db.users.getUsers()}
   );
 };
 
-controller.create = function(req, res, next) {
-  res.send('user create');
+controller.show = function(req, res, next) {
+  return res.render('user/show',
+    {user: db.users.findOne({_id: req.param('id')}) }
+  );
 };
+
+// create new user
+
+controller.create = function(req, res, next) {
+  
+  var User = db.main.model('User'),
+      user = new User(req.param('user'));
+  
+  user.save(function(err) {
+    console.log('saving in controller...');
+    if(!err) {
+      console.log('saved');
+    } else {
+      console.log(err + ' <- error saving');
+    }
+  });
+  return res.render('user/index', {users: db.users.getUsers()});
+};
+
+// load user form
 
 controller.edit = function(req, res, next) {
-  res.send('user edit');
+  var user = db.users.findById(req.param('id'));
+  return res.render('user/edit', {user: user})
 };
 
+// update user info
+
+controller.update = function(req, res, next) {
+  var user = req.body.user;
+  
+  return db.users.findOne({ _id: user._id}, function(err, _user) {
+    if(err) return next(err);
+
+    _user.name.first = user.name.first;
+    _user.name.last = user.name.last;
+    _user.email = user.email;
+
+    _user.save(function(err) {
+      if(err) return next(err);
+    });
+    
+    return res.redirect('/user/' + user._id);
+  });
+};
+
+// delete user
+
 controller.delete = function(req, res, next) {
-  res.send('user delete');
+  return db.users.findOne({ _id: req.param('id') }, function(err, _user) {
+    if(err) return next(err);
+    _user.remove();
+    return res.redirect('/user');
+  });
 };
--- config/routes.js
+++ config/routes.js
@@ -11,8 +11,11 @@ module.exports = function(app) {
 
   app.get('/user', user.index);
   app.get('/user/create', user.create);
-  app.get('/user/:id', user.index);
+  app.get('/user/:id', user.show);
   app.get('/user/:id/edit', user.edit);
   app.get('/user/:id/delete', user.delete);
+  
+  app.post('/user/edit', user.update);
+
 };
 
