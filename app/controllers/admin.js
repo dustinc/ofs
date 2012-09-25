56dda12963ac675b26be441381c0236d4b992c19
Dustin <coffey.dustin@gmail.com>
2012-09-25 18:02:47 -0400

    Add Admin controllers and views.

---
+++ app/controllers/admin.js
@@ -0,0 +1,38 @@
+
+// Globals
+
+var controller = {},
+    app,
+    db,
+    scripts = [];
+
+// Constructor
+
+module.exports = function(_app) {
+  app = _app;
+  db = app.set('db');
+  scripts.push('/scripts/admin.js');
+  controller.scripts = scripts;
+  return controller;
+};
+
+controller.index = function(req, res, next) {
+  return res.render('admin/index', {scripts: scripts});
+};
+
+controller.users = function(req, res, next) {
+  return res.render('admin/users',
+    {users: db.users.getUsers(), scripts: scripts}
+  );
+};
+
+controller.lookups = function(req, res, next) {
+  return res.render('admin/lookups',
+    {lookups: db.lookups.find(), scripts: scripts}
+  );
+};
+
+
+controller.quick_edit_li = function(req, res, next) {
+  return res.render('admin/quick_edit_li', {lookup_value: ''});
+};
