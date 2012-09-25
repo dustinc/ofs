ed48a8b4ae46af6d74333b12525d87c4e4fb1e73
Dustin <coffey.dustin@gmail.com>
2012-09-25 17:33:24 -0400

    Update Lookup Model.
    Add statics and methods.

--- app/models/lookup.js
+++ app/models/lookup.js
@@ -7,4 +7,52 @@ var Lookup = module.exports = new Schema({
   values: Array
 });
 
+Lookup.statics.loadTeacherLookups = function(callback) {
+  this.where('name').in(['Degrees', 'Service Types']).exec(function(err, lookups) {
+    return lookups;
+  });
+};
+
+Lookup.statics.load = function(name, callback) {
+  return this.findOne({'name': name}).select('name', 'values');
+};
+
+Lookup.statics.loadProfileLookups = function(profile_type, callback) {
+  var l_list = [];
+
+  switch(profile_type) {
+    case 'Teacher':
+      l_list = ['Degrees', 'Service Types'];
+      break;
+    case 'Institution':
+      l_list = [];
+      break;
+  }
+
+  this.find({'name': {$in: l_list}}, function(err, _lookups) {
+    var teacher_lookups = {};
+
+    if(err) return next(err);
+
+    _lookups.forEach(function(lookup) {
+      teacher_lookups[lookup.name.toLowerCase().replace(' ', '_')] = lookup;
+    });
+
+    callback(err, teacher_lookups);
+  });
+
+};
+
 Lookup.plugin(TimeStamp);
+
+Lookup.pre('init', function(next) {
+  next();
+});
+
+Lookup.pre('save', function(next) {
+  next();
+});
+
+Lookup.pre('remove', function(next) {
+  next();
+});
