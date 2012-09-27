d2b14de7b1eb775f612c9ca78d9413ca190f855d
Dustin <coffey.dustin@gmail.com>
2012-09-27 11:40:54 -0400

    Clean up Lookup Model.
    Mostly remove unwanted statics.

--- app/models/lookup.js
+++ app/models/lookup.js
@@ -2,48 +2,21 @@
 var Schema = require('mongoose').Schema,
     TimeStamp = require('./timestamp');
 
+/*
+ * Lookup Schema
+ */
+
 var Lookup = module.exports = new Schema({
-  name: { type: String, required: true, index: { unique: true }},
-  values: Array
+  name: { type: String, required: true, index: { unique: true } },
+  values: []
 });
 
-Lookup.statics.loadTeacherLookups = function(callback) {
-  this.where('name').in(['Degrees', 'Service Types']).exec(function(err, lookups) {
-    return lookups;
-  });
-};
-
-Lookup.statics.load = function(name, callback) {
-  return this.findOne({'name': name}).select('name', 'values');
-};
-
-Lookup.statics.loadProfileLookups = function(profile_type, callback) {
-  var l_list = [];
-
-  switch(profile_type) {
-    case 'Teacher':
-      l_list = ['Degrees', 'Service Types'];
-      break;
-    case 'Institution':
-      l_list = [];
-      break;
-  }
-
-  this.find({'name': {$in: l_list}}, function(err, _lookups) {
-    var teacher_lookups = {};
-
-    if(err) return next(err);
-
-    _lookups.forEach(function(lookup) {
-      teacher_lookups[lookup.name.toLowerCase().replace(' ', '_')] = lookup;
-    });
-
-    callback(err, teacher_lookups);
-  });
-
-};
+// timestamp plugin
 
 Lookup.plugin(TimeStamp);
+
+
+// PRE Hooks
 
 Lookup.pre('init', function(next) {
   next();
@@ -55,4 +28,11 @@ Lookup.pre('save', function(next) {
 
 Lookup.pre('remove', function(next) {
   next();
+});
+
+
+// POST Hooks
+
+Lookup.post('init', function() {
+
 });
