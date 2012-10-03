ecf460cbbcead14ee08c49c56a94bf90eb96deb7
Dustin <coffey.dustin@gmail.com>
2012-10-02 20:51:36 -0400

    Add basic adjunct search feature.

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -26,3 +26,24 @@ controller.signup = function(req, res, next) {
   var user_types = db.lookups.findOne({name: 'User Types'});
   return res.render('signup', {user_types: user_types});
 };
+
+controller.adjunct_search = function(req, res, next) {
+
+  var lookups = db.lookups.findOne({name: 'Teacher Lookups'}),
+      query = db.teachers.find().populate('user_id'),
+      post = req.param('adjunct');
+
+  if(post) {
+    for(var key in post) {
+      if(post[key] != '') {
+        query.where(key, post[key]);
+      }
+    }
+  }
+
+  return res.render('adjunct_search', {lookups: lookups, search_results: query.exec()});
+};
+
+controller.job_search = function(req, res, next) {
+  return res.send('job search');
+};
