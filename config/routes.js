ea7b06fa12f2884642fc2c42d163ab7e76132754
Dustin <coffey.dustin@gmail.com>
2012-10-09 11:28:38 -0400

    Update Routes.
    Add some route middleware to check auth. Closes #18
    Organize routes by need of auth middleware. Closes #20

--- config/routes.js
+++ config/routes.js
@@ -5,48 +5,112 @@ module.exports = function(app) {
       admin = require('../app/controllers/admin')(app),
       user = require('../app/controllers/user')(app),
       lookup = require('../app/controllers/lookup')(app),
-      db = app.set('db');
+      db = app.set('db'),
+
+      isLoggedIn = function(req, res, next) {
+        var s_user = (req.session.user) ? req.session.user : false;
+        res.local('s_user', s_user);
+        next();
+      },
+
+      authRequired = function(req, res, next) {
+        if(!req.session.user) {
+          res.status(403);
+          return next(new Error('User Not Authorized'));
+        }
+        next();
+      },
+
+      isAdmin = function(req, res, next) {
+        if(!req.session.user.is_admin) {
+          res.status(403);
+          return next(new Error('Must be Admin to Access Page'));
+        }
+        next();
+      },
+
+      isThisUser = function(req, res, next) {
+        if(!req.session.user.is_admin && req.params.user_id != req.session.user._id) {
+          res.status(404);
+          return next(new Error('Sorry, the page you are you looking for does not exist.'));
+        }
+        next();
+      },
+
+      authenticatedAdmin = [authRequired, isAdmin],
+
+      authenticatedUser = [authRequired, isThisUser];
+
+
+
+
+
+  /*
+   * Viewable By All
+  */
+
+
+  // set local auth var for all pages.
+  app.get('/*', isLoggedIn);
+
+  // login
+  app.post('/login', main.login);
+  app.get('/logout', main.logout);
 
   // general
   app.get('/', main.index);
   app.get('/about', main.load.bind(null, 'about'));
-  app.get('/signup', main.signup);
   app.get('/forums', function(req, res, next) {return res.send('forums');});
   app.get('/templates-and-tutorials', function(req, res, next) {return res.send('templates-and-tutorials');});
   app.get('/personalized-help', function(req, res, next) {return res.send('personalized-help');});
 
   // searches
-  app.get('/search/adjunct', main.adjunct_search);
-  app.get('/search/jobs', main.job_search);
+  app.get('/search/adjunct', main.adjunct_search);// add seach middleware to show limited search/results to unauthorized users
+  app.get('/search/jobs', main.job_search);// same as adjunct search
+
+  // create new user
+  app.get('/signup', main.signup);
+  app.post('/user/create', user.create);
+
+
+  /*
+   * Requires Authentication
+  */
+
 
   // admin
-  app.get('/admin', admin.index);
-  app.get('(?:\/admin)?/users?', user.index);
-  app.get('/admin/quick_edit_li', admin.quick_edit_li);
-
-  // users
-  app.get('/user/:user_id', user.show);
-  app.get('/user/:user_id/edit', user.edit);
-  app.get('/user/:user_id/save', user.save);
-  app.get('/user/:user_id/delete', user.delete);
-  app.post('/user/create', user.create);
-  app.post('/user/:user_id/update', user.update);
-
-  //user profile
-  app.get('/user/:user_id/profile', user.profile);
-  app.get('/user/:user_id/profile/edit', user.profile.edit);
-  app.post('/user/:user_id/profile/save', user.profile.save);
+  app.get('/admin', authenticatedAdmin, admin.index);
+  app.get('/admin/users?', user.index);
+  app.get('/admin/quick_edit_li', admin.quick_edit_li);// remove this
 
   // lookups
-  app.get('(?:\/admin)?/lookups?', lookup.index);
-  app.get('(?:\/admin)?/lookups?/create', lookup.create);
-  app.get('(?:\/admin)?/lookups?/:name', lookup.show);
-  app.get('(?:\/admin)?/lookups?/:name/edit', lookup.edit);
-  app.post('(?:\/admin)?/lookups?/:name/update', lookup.update);
-  app.get('(?:\/admin)?/lookups?/:name/remove', lookup.delete);
+  app.get('/admin/lookups?', authenticatedAdmin, lookup.index);
+  app.get('/admin/lookups?/create', authenticatedAdmin, lookup.create);
+  app.get('/admin/lookups?/:name', authenticatedAdmin, lookup.show);
+  app.get('/admin/lookups?/:name/edit', authenticatedAdmin, lookup.edit);
+  app.post('/admin/lookups?/:name/update', authenticatedAdmin, lookup.update);
+  app.get('/admin/lookups?/:name/remove', authenticatedAdmin, lookup.delete);
+
+  // users
+  app.get('/user/:user_id', authenticatedUser, user.show);
+  app.get('/user/:user_id/edit', authenticatedUser, user.edit);
+  app.get('/user/:user_id/save', authenticatedUser, user.save);
+  app.get('/user/:user_id/delete', authenticatedUser, user.delete);
+  app.post('/user/:user_id/update', authenticatedUser, user.update);
+
+  //user profile
+  app.get('/user/:user_id/profile', authenticatedUser, user.profile);
+  app.get('/user/:user_id/profile/edit', authenticatedUser, user.profile.edit);
+  app.post('/user/:user_id/profile/save', authenticatedUser, user.profile.save);
+
+
+  /*
+   * End of the trail, catch-all 404
+  */
 
   app.get('/*', function(req, res, next) {
-    res.status(404).render('error', {error: '404'});
+    res.status(404);
+    next(new Error('Sorry, the page you are you looking for does not exist.'));
   });
 
 };
