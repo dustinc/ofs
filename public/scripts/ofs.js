d3bc2a3127e27d99b234d4811b91d01997ec0f46
Dustin <coffey.dustin@gmail.com>
2012-10-31 12:05:43 -0400

    Add sign up form soft validation.

--- public/scripts/ofs.js
+++ public/scripts/ofs.js
@@ -81,4 +81,34 @@ $('.item_list').on('click', '.remove_item', function() {
 });
 
 
+// Sign up form validation
+
+$('#signupform').submit(function() {
+  var $form = $(this),
+      error = false;
+
+  // remove existing error messages
+  $form.find('span.error').remove();
+
+  $form.find('.notempty').each(function() {
+    if($(this).val() == '') {
+      $(this).after('<span class="error">Please Enter '+$(this).prev('label').text()+'</span>');
+      error = true;
+    }
+  });
+
+
+  if($form.find('#pw').val() != '') {
+    if($form.find('#pw').val() != $form.find('#cpw').val()) {
+      $form.find('#pw').after('<span class="error">Password Does Not Match</span>');
+      error = true;
+    }
+  } else {
+    $form.find('#pw').after('<span class="error">Please Enter Password</span>');
+    error = true;
+  }
+
+  if(error) return false;
+});
+
 });
